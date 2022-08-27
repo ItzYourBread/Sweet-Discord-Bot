d0e605ab9dd94ee5e686ba2525f26ea0a33e5b33
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-08-28 03:57:03 +0600

    Done

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    botID: "814151198054547506",
-    botSecret: "oCGmu_MuGC2jgzgu9fMFBO4DEUiRH4dH",
-    inviteURL: "https://discord.com/oauth2/authorize?client_id=814151198054547506&permissions=1241211464951&redirect_uri=https%3A%2F%2Fsweet.cf&response_type=code&scope=applications.commands%20email%20identify%20guilds%20bot",
-    supportServer: "https://discord.gg/KBgdDG9TPc",
+    botID: "YOUR BOT CLIENT ID",
+    botSecret: "YOUR BOT SECRET CLIENT ID",
+    inviteURL: "YOUR BOT INVITE LINK",
+    supportServer: "SUPPORT SERVER",
     embedColor: "#FFC1C1",  
-    MongoURL: "mongodb+srv://Sweet:djarif100@sweet-mongo.qvysg.mongodb.net/?retryWrites=true&w=majority",
+    MongoURL: "YOUR MONGO DATABASE",
     success: "<:sweetdone:989146558845050990>",
     fail: "<:sweetdeny:989146607691890689>",
 }
