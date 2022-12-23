e4e6281f5e7663b15c50b1280f74019d4fae18d8
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-12-23 16:22:02 +0600

    Update index.js

--- handlers/index.js
+++ handlers/index.js
@@ -71,7 +71,7 @@ const loadSlashCommands = async function (client) {
         //    .commands.set(slash);
 
         // Register Slash Commands for all the guilds
-        await client.application.commands.set([]);
+        await client.application.commands.set(slash]);
     });
 };
 
